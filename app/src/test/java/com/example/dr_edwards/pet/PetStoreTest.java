bc9b1ebb6de78d7c5d08bcef7a90d7034d13867a
Martin Almaraz <martin@twinhealth.com>
2024-10-17 14:30:04 -0500

    updated

--- app/src/test/java/com/example/dr_edwards/pet/PetStoreTest.java
+++ app/src/test/java/com/example/dr_edwards/pet/PetStoreTest.java
@@ -6,12 +6,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class PetStoreTest {
 
@@ -49,6 +54,21 @@ public class PetStoreTest {
     }
 
     @Test
+    public void testBuyPet_oldStyle() {
+        // Arrange
+        Pet dog = new Dog();
+        PetStore petStore = new PetStore();
+        int money = 5;
+        petStore.sellPet(dog);
+
+        // Act
+        Pet actual = petStore.buyPet(money);
+
+        // Assert
+        assertEquals(dog, actual);
+    }
+
+    @Test
     public void testBuyPet_cat() {
         // Arrange
         Pet cat = new Cat();
@@ -62,5 +82,24 @@ public class PetStoreTest {
 
         // Assert
         assertEquals(cat, actual);
+    }
+
+    @Test
+    public void testBuyPet_catNotEnoughMoney() {
+        // Arrange
+        Pet cat = new Cat();
+        List<Pet> pets = new ArrayList<>();
+        pets.add(cat);
+        PetValueCalculator petValueCalculatorMock = mock(PetValueCalculator.class);
+        when(petValueCalculatorMock.calculateValueOfPet(eq(cat))).thenReturn(10);
+        PetStore petStore = new PetStore(pets, petValueCalculatorMock, new CashRegister(10));
+        int money = 10;
+
+        // Act
+        Pet actual = petStore.buyPet(money);
+
+        // Assert
+        assertEquals(cat, actual);
+        verify(petValueCalculatorMock, times(1)).calculateValueOfPet(eq(cat));
     }
 }
