c631e95874bf5063a66e30975a616e493e9b9fd1
Martin Almaraz <martin@twinhealth.com>
2024-10-17 00:22:26 -0500

    fixed instanceof

--- app/src/main/java/com/example/dr_edwards/pet/PetStore.java
+++ app/src/main/java/com/example/dr_edwards/pet/PetStore.java
@@ -33,9 +33,9 @@ public class PetStore {
     }
 
     private int calculateValueOfPet(Pet pet) {
-        if (pet instanceof Cat) {
+        if (pet.speak().equals("Meow")) {
             return 10;
-        } else if (pet instanceof Dog) {
+        } else if (pet.speak().equals("Woof")) {
             return 5;
         } else {
             throw new RuntimeException("I don't know this animal!" + pet.speak());
